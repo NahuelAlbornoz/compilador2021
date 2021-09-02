dd64a956ea69b3d2add41e4ae8afafe64267fbf8
NahuelAlbornoz <39098282+NahuelAlbornoz@users.noreply.github.com>
2021-09-02 12:41:09 -0300

    Update an.lex.c
    lectura del archivo

--- an.lex.c
+++ an.lex.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
 
 int main(){
-    printf("holi");
-    getch();
-    system("pause");
-    return 0;
+    
+    FILE * input = fopen("input.txt", "rb");
+    char caracter;
+
+    if(input != NULL){
+        while (feof(input) == 0)
+        {
+            caracter = fgetc(input);
+            printf("%c", caracter);
+        }
+        fclose(input);
+        printf("\nFin de la lectura del archivo\n");
+    }
+    else if(input == NULL){
+        perror("Ocurrio un error al intentar abrir el archivo\n");
+        return 1;
+    }
 }
